479d215e984c579b7e61a7c7e90798e393284e63
BuzzKill Master <buzzkillmaster@proton.me>
2023-04-23 12:57:27 +0200

    add models for menu and booking

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models import Model, CharField, IntegerField, DateField, DecimalField
+
 
 # Create your models here.
+class Booking(Model):
+    name = CharField(max_length=255)
+    no_of_guests = IntegerField()
+    booking_date = DateField()
+
+
+class Menu(Model):
+    title = CharField(max_length=255)
+    price = DecimalField(max_digits=10, decimal_places=2)
+    inventory = IntegerField()
